a8fae6625c2bfd6db0cd2700315392f2124c684b
Pau Llabrés <pablo.llabres@gmail.com>
2016-11-18 00:09:14 +0100

    He afegit una TERCERA linea de println al codi principal

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,6 @@ public class Main {
  public static void printHola(){
  System.out.println("Hola món! i Hola Xavi!");
   System.out.println("Qué tal estas?");
+  System.out.println("Molt béi tu?");
  }
 }
